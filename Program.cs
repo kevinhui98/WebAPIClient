a3d771c29f6f3c2a20aa7ee79e59a087ad715d51
kevin <kevinhui98@gmail.com>
2022-02-26 23:22:10 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebAPIClient
+{
+    class MainClass
+    {
+        //private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //await Processrepositories();
+
+        }
+    }
+}
